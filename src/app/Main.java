7dfcca8a9ba2869cf7b9a9a555e5d0003d3fa12c
lhgoulart <lhgoulart92@yahoo.com.br>
2018-10-15 19:05:07 -0300

    First class: Position

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,15 @@
 
 package app;
 
+import boardgame.Position;
+
 
 public class Main {
 
     public static void main(String[] args) {
         
-        System.out.println("!");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
     
 }
